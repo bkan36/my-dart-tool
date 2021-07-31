871b29d52d3186f4ad7f924ea27fd37b2eaf6c7b
bkan36 <bkan3060@gmail.com>
2021-07-31 10:47:57 +0200

    file builder v2 | files pre-filled

--- bin/file_builder.dart
+++ bin/file_builder.dart
@@ -2,24 +2,59 @@ import 'dart:io';
 
 void main(List<String> args) {
   if (args.isEmpty) exit(1);
-  
+
   final env = Platform.environment;
-  final absolutePath = '${env['PWD']}/${args[0]}';
+  final relativePath = '${args[0]}';
   var ext = 'dart';
-  
+
   if (args.length > 1 && ext != args[1]) ext = args[1];
-  
+
   final nameDir = Uri.parse(args[0]).pathSegments.first;
 
   Directory(nameDir).createSync();
-  
+
   const mvp = [
     '_controller',
     '_presenter',
     '_view',
   ];
 
-  // ignore: curly_braces_in_flow_control_structures
-  for (var fn in mvp ) File('$absolutePath/$nameDir$fn.$ext').createSync();
+  mvp.forEach((fn) {
+    var pathFile = '$relativePath/$nameDir$fn.$ext';
+    var className = '';
+
+    File(pathFile).createSync();
+
+    nameDir.split('_').forEach((n) => className += n.toCamelCase());
+    className += fn.substring(1).toCamelCase();
+
+    if (fn != '_view') {
+      File(pathFile).writeAsStringSync('''
+class $className {
+  static final $className _singleton =
+    $className._privateConstructor();
+  factory $className() => _singleton;
+  $className._privateConstructor();
+}
+''');
+    } else {
+      File(pathFile).writeAsStringSync('''
+import 'package:flutter/material.dart';
+
+class $className extends StatelessWidget {
+  const $className({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+        ''');
+    }
+  });
   exit(0);
+}
+
+extension CamelCase on String {
+  String toCamelCase() => this[0].toUpperCase() + substring(1);
 }
